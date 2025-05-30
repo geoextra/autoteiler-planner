e6a9fb5e8139f79b8a32286a96566d5ebb37adff
Moritz Gehlhaar <moritz.gehlhaar@posteo.de>
2025-05-30 10:09:09 +0200

    Disable Sentry in dev mode

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -7,29 +7,32 @@ import {
 	contextLinesIntegration
 } from '@sentry/sveltekit';
 import * as Sentry from '@sentry/sveltekit';
+import { dev } from '$app/environment';
 
-Sentry.init({
-	dsn: 'https://658820d34b7baed89f29b317010d7612@o4509406541512704.ingest.de.sentry.io/4509406542757968',
+if (!dev) {
+	Sentry.init({
+		dsn: 'https://658820d34b7baed89f29b317010d7612@o4509406541512704.ingest.de.sentry.io/4509406542757968',
 
-	tracesSampleRate: 1.0,
+		tracesSampleRate: 1.0,
 
-	// This sets the sample rate to be 10%. You may want this to be 100% while
-	// in development and sample at a lower rate in production
-	replaysSessionSampleRate: 1.0,
+		// This sets the sample rate to be 10%. You may want this to be 100% while
+		// in development and sample at a lower rate in production
+		replaysSessionSampleRate: 1.0,
 
-	// If the entire session is not sampled, use the below sample rate to sample
-	// sessions when an error occurs.
-	replaysOnErrorSampleRate: 1.0,
+		// If the entire session is not sampled, use the below sample rate to sample
+		// sessions when an error occurs.
+		replaysOnErrorSampleRate: 1.0,
 
-	// If you don't want to use Session Replay, just remove the line below:
-	integrations: [
-		replayIntegration(),
-		replayCanvasIntegration(),
-		captureConsoleIntegration(),
-		browserProfilingIntegration(),
-		contextLinesIntegration()
-	]
-});
+		// If you don't want to use Session Replay, just remove the line below:
+		integrations: [
+			replayIntegration(),
+			replayCanvasIntegration(),
+			captureConsoleIntegration(),
+			browserProfilingIntegration(),
+			contextLinesIntegration()
+		]
+	});
+}
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
 export const handleError = handleErrorWithSentry();
